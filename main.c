94da72bf0af6d37166933852148e85022e9b1a47
Gamaliel Adun <adungamaliel@gmail.com>
2022-12-17 08:23:27 +0000

    added tests

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ *
+ *
+ *
+ */
+
+
+
+int main(int argc, char **argv)
+{
+
+	if (argc == 2)
+	{
+		cmd = getline(
+	printf("%s\n", argv[0]);
+	printf("%s\n", argv[1]);
+	printf("%d\n", argc);
+}
